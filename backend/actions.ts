bbe6cffa3d47d9e3b91355417d3a6d8d6246ba67
André Samuelsson <andresamuelsson94@gmail.com>
2019-12-07 15:14:46 +0100

    Woo

--- backend/actions.ts
+++ backend/actions.ts
@@ -1,4 +1,10 @@
 export const AUTHENTICATE_USER = "AUTHENTICATE_USER";
+export const VOTE = "VOTE";
+
+enum DirectionVote {
+  UP = 1,
+  DOWN = -1
+}
 
 export const registerUser = (username: string, password: string) => {
   return {
@@ -7,3 +13,26 @@ export const registerUser = (username: string, password: string) => {
     password
   };
 };
+
+export const vote = (
+  token: string,
+  voteDirection: DirectionVote,
+  songId: string,
+  userId: string
+): VoteAction => {
+  return {
+    type: VOTE,
+    userId,
+    token,
+    voteDirection,
+    songId
+  };
+};
+
+export interface VoteAction {
+  type: typeof VOTE;
+  token: string;
+  userId: string;
+  voteDirection: DirectionVote;
+  songId: string;
+}
--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,7 @@
 import uuid from "uuid/v4";
 import WebSocket from "ws";
-import { createStore } from "redux";
-import { AUTHENTICATE_USER } from "./actions";
+import { createStore, combineReducers } from "redux";
+import { AUTHENTICATE_USER, VOTE, VoteAction } from "./actions";
 
 interface User {
   name: string;
@@ -17,36 +17,44 @@ interface Vote {
 interface Song {
   id: string;
   name: string;
-  votes: Vote[];
+  votes: Record<string, number>;
 }
 
 interface JukeboxState {
-  songs: Song[];
-  users: {
-    [key: string]: User;
-  };
+  songs: Record<string, Song>;
 }
 
-interface FrontendState {
-  songs: Song[];
-}
-
-const initialState = {
-  songs: [
-    {
-      id: "piano_man",
-      name: "Piano man",
-      votes: []
+const initialState: RootState = {
+  mutationCount: 0,
+  jukebox: {
+    songs: {
+      piano_man: {
+        id: "piano_man",
+        name: "Piano man",
+        votes: {}
+      }
     }
-  ],
+  },
   users: {
-    tejp: { name: "tejp", password: "apabepa" }
+    "123123": { name: "tejp", password: "apabepa", token: "123123" }
   }
 };
 
-const jukebox = (state = initialState, action: any) => {
+interface RootState {
+  jukebox: JukeboxState;
+  mutationCount: number;
+  users: Record<string, User>;
+}
+
+const mutationCountReducer = (state: number = 0, action: any): number => {
+  return state + 1;
+};
+
+const userReducer = (
+  state: Record<string, User> = initialState.users,
+  action: any
+): Record<string, User> => {
   switch (action.type) {
-    // we'll handle only one action: when new tweets arrive
     case AUTHENTICATE_USER:
       const { name, password } = action;
       return {
@@ -61,7 +69,37 @@ const jukebox = (state = initialState, action: any) => {
   }
 };
 
-const store = createStore(jukebox);
+const jukebox = (
+  state: JukeboxState = initialState.jukebox,
+  action: VoteAction
+): JukeboxState => {
+  switch (action.type) {
+    case VOTE:
+      return {
+        ...state,
+        songs: {
+          ...state.songs,
+          [action.songId]: {
+            ...state.songs[action.songId],
+            votes: {
+              ...state.songs[action.songId].votes,
+              [action.userId]: action.voteDirection
+            }
+          }
+        }
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(
+  combineReducers<RootState>({
+    jukebox,
+    mutationCount: mutationCountReducer,
+    users: userReducer
+  })
+);
 
 console.log("Running jukebox backend");
 
@@ -69,6 +107,7 @@ const wss = new WebSocket.Server({ host: "0.0.0.0", port: 8080 });
 let connections: WebSocket[] = [];
 wss.on("connection", function connection(ws) {
   connections.push(ws);
+  ws.send(JSON.stringify(store.getState()));
 
   ws.on("message", function incoming(message) {
     console.log("received: %s", message);
